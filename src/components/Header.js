e78ab2edab6542c23985602fd60dfde97cf0ac63
yilinng <31823305+yilinng@users.noreply.github.com>
2020-11-06 13:41:51 +0800

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,8 +5,10 @@ const Header = ({shows, popUp, trackHandle}) => {
 
 	//Sorting an array of objects by property values
 	//const showFiltered = shows.sort((a, b) => parseFloat(a.rating.average) - parseFloat(b.rating.average));
-	const byRating  = shows.sort(function(a,b) {return (a.rating.average >b.rating.average) ? 1 : ((b.rating.average >a.rating.average) ? -1 : 0);} );
-
+	//const byRating  = shows.sort(function(a,b) {return (a.rating.average >b.rating.average) ? 1 : ((b.rating.average >a.rating.average) ? -1 : 0);} );
+	const byRating = shows.sort(function (a, b) {
+	  return a.rating.average - b.rating.average;
+	});
  	const showList = byRating.slice(237,240).length ? (
 	 Array.from(byRating.slice(237,240)).map(show => {
 	 	return(
