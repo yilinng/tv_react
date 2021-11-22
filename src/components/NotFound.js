f0be317572d49dce531f2c67e5eddffdd5c00a21
yilinng <wei9912@gmail.com>
2021-11-22 13:58:27 +0800

    edit env

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function NotFound() {
     return (
         <div>
-            <h1 className="text-white m-auto">not Found..</h1>  
+            <h1 className="text-white m-auto">not Found...404</h1>  
         </div>
     )
 }
